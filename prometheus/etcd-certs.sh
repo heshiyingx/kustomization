7e3387be418789c0be7287a9e9179eb32e701f9a
heshiying <heshiyingx@126.com>
2023-03-16 03:01:21 +0800

    add configmap

---
+++ prometheus/etcd-certs.sh
@@ -0,0 +1,2 @@
+#!/bin/env bash
+kubectl create secret generic etcd-certs --from-file=/etc/kubernetes/pki/etcd/healthcheck-client.crt --from-file=/etc/kubernetes/pki/etcd/healthcheck-client.key --from-file=/etc/kubernetes/pki/etcd/ca.crt -n kube-devops
